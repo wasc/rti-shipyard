13766cb6d4f6ad990b656bf7bdd98add9df1587d
Nikolaos Souris <nikolaos.souris@rwth-aachen.de>
2024-02-18 18:07:36 +0100

    .

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,17 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <ThemeProvider theme={darkTheme}>
+    <CssBaseline />
+    <App />
+  </ThemeProvider>
+);
